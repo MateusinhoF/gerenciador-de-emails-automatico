341c615bf4a80ab9584c8ddafc4077a55fdfcbf9
mateus-taiga <mateus.f.g.dacosta@outlook.com>
2023-09-28 11:51:45 -0300

    adicionado sudo

--- run.sh
+++ run.sh
@@ -6,20 +6,20 @@ if ! command -v apache2 &> /dev/null; then
     sudo apt-get install php-xml #php-curl php-pdo
     #sudo chmod -R 777 /var/www
 
-    sed -i "s/;extension=curl/extension=curl/" /etc/php/8.1/cli/php.ini
-    sed -i "s/;extension=curl/extension=curl/" /etc/php/8.1/apache2/php.ini
+    sudo sed -i "s/;extension=curl/extension=curl/" /etc/php/8.1/cli/php.ini
+    sudo sed -i "s/;extension=curl/extension=curl/" /etc/php/8.1/apache2/php.ini
 
-    sed -i "s/;extension=pdo_mysql/extension=pdo_mysql/" /etc/php/8.1/cli/php.ini
-    sed -i "s/;extension=pdo_mysql/extension=pdo_mysql/" /etc/php/8.1/apache2/php.ini
+    sudo sed -i "s/;extension=pdo_mysql/extension=pdo_mysql/" /etc/php/8.1/cli/php.ini
+    sudo sed -i "s/;extension=pdo_mysql/extension=pdo_mysql/" /etc/php/8.1/apache2/php.ini
 
-    sed -i "s/;extension=fileinfo/extension=fileinfo/" /etc/php/8.1/cli/php.ini
-    sed -i "s/;extension=fileinfo/extension=fileinfo/" /etc/php/8.1/apache2/php.ini
+    sudo sed -i "s/;extension=fileinfo/extension=fileinfo/" /etc/php/8.1/cli/php.ini
+    sudo sed -i "s/;extension=fileinfo/extension=fileinfo/" /etc/php/8.1/apache2/php.ini
 
-    sed -i "s/;extension=mbstring/extension=mbstring/" /etc/php/8.1/cli/php.ini
-    sed -i "s/;extension=mbstring/extension=mbstring/" /etc/php/8.1/apache2/php.ini
+    sudo sed -i "s/;extension=mbstring/extension=mbstring/" /etc/php/8.1/cli/php.ini
+    sudo sed -i "s/;extension=mbstring/extension=mbstring/" /etc/php/8.1/apache2/php.ini
 
-    sed -i "s/;extension=openssl/extension=openssl/" /etc/php/8.1/cli/php.ini
-    sed -i "s/;extension=openssl/extension=openssl/" /etc/php/8.1/apache2/php.ini
+    sudo sed -i "s/;extension=openssl/extension=openssl/" /etc/php/8.1/cli/php.ini
+    sudo sed -i "s/;extension=openssl/extension=openssl/" /etc/php/8.1/apache2/php.ini
 
 fi
 
