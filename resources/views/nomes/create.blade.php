18afb253d89b643a4a5db536b8549e06168c3640
mateus-taiga <mateus.f.g.dacosta@outlook.com>
2023-10-16 11:38:57 -0300

    nomes

--- resources/views/nomes/create.blade.php
+++ resources/views/nomes/create.blade.php
@@ -1,25 +1,28 @@
-<x-template titulodapagina="" tituloHeader="Cadastrar Nomes">
+<x-template titulodapagina="Cadastrar Nomes" tituloHeader="Cadastrar Nomes">
 
-    <div class="d-flex justify-content-around">
-        <x-link href="{{route('nomes.index')}}" texto="Voltar"/>
-        <x-link href="{{route('logout')}}" texto="Sair"/>
-    </div>
+    <div class="mx-auto col-md-12 bg-secondary">
 
-    <form action="{{route('nomes.store')}}" method="post" class="p-4 p-md-5 border rouded-3 bg-light">
-
-        @csrf
-
-        <x-input nome="nome1" texto="Nome 1" tipo="text"/>
-        <x-input nome="nome2" texto="Nome 2" tipo="text"/>
-        <x-input nome="nome3" texto="Nome 3" tipo="text"/>
-        <x-input nome="nome4" texto="Nome 4" tipo="text"/>
-        <x-input nome="nome5" texto="Nome 5" tipo="text"/>
-
-        <div class="d-flex justify-content-center">
-            <input type="submit" value="Cadastrar Nomes" class="btn btn-primary">
+        <div class="d-flex justify-content-around m-3">
+            <x-link href="{{route('nomes.index')}}" texto="Voltar"/>
+            <x-link-sair/>
         </div>
 
-    </form>
+        <form action="{{route('nomes.store')}}" method="post" class="m-3 p-4 color-forms-background">
 
-    <x-errors/>
+            @csrf
+
+            <x-input nome="nome1" texto="Nome 1" tipo="text"/>
+            <x-input nome="nome2" texto="Nome 2" tipo="text"/>
+            <x-input nome="nome3" texto="Nome 3" tipo="text"/>
+            <x-input nome="nome4" texto="Nome 4" tipo="text"/>
+            <x-input nome="nome5" texto="Nome 5" tipo="text"/>
+
+            <div class="d-flex justify-content-center">
+                <input type="submit" value="Cadastrar Nomes" class="btn btn-primary">
+            </div>
+
+        </form>
+
+        <x-errors/>
+    </div>
 </x-template>
--- resources/views/nomes/index.blade.php
+++ resources/views/nomes/index.blade.php
@@ -1,39 +1,41 @@
-<x-template titulodapagina="" tituloHeader="Nomes Index">
+<x-template titulodapagina="Nomes" tituloHeader="Nomes">
 
-    <div class="d-flex justify-content-around">
-        <x-link href="{{route('nomes.create')}}" texto="Cadastrar nomes"/>
-        <x-link href="{{route('paraenviar.index')}}" texto="Voltar"/>
-        <x-link href="{{route('logout')}}" texto="Sair"/>
-    </div>
+    <div class="mx-auto col-md-12 bg-secondary">
+        <div class="d-flex justify-content-around m-3">
+            <x-link href="{{route('nomes.create')}}" texto="Cadastrar nomes"/>
+            <x-link href="{{route('paraenviar.index')}}" texto="Voltar"/>
+            <x-link-sair/>
+        </div>
 
-    <div>
-        <table class="table table-striped table-hover">
-            <thead>
-            <tr>
-                <th>Nome 1</th>
-                <th>Nome 2</th>
-                <th>Nome 3</th>
-                <th>Nome 4</th>
-                <th>Nome 5</th>
-                <th>Opções</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($nomes as $nome)
+        <div>
+            <table class="table table-striped table-hover">
+                <thead>
                 <tr>
-                    <td>{{$nome->nome1}}</td>
-                    <td>{{$nome->nome2}}</td>
-                    <td>{{$nome->nome3}}</td>
-                    <td>{{$nome->nome4}}</td>
-                    <td>{{$nome->nome5}}</td>
-                    <td>
-                        <x-link href="{{route('nomes.edit', ['id'=>$nome->id])}}" texto="Editar"/>
-                        <x-link href="{{route('nomes.destroy', ['id'=>$nome->id])}}" texto="Excluir"/>
-                    </td>
+                    <th>Nome 1</th>
+                    <th>Nome 2</th>
+                    <th>Nome 3</th>
+                    <th>Nome 4</th>
+                    <th>Nome 5</th>
+                    <th>Opções</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach ($nomes as $nome)
+                    <tr>
+                        <td>{{$nome->nome1}}</td>
+                        <td>{{$nome->nome2}}</td>
+                        <td>{{$nome->nome3}}</td>
+                        <td>{{$nome->nome4}}</td>
+                        <td>{{$nome->nome5}}</td>
+                        <td>
+                            <x-link-editar href="{{route('nomes.edit', ['id'=>$nome->id])}}"/>
+                            <x-link-excluir href="{{route('nomes.destroy', ['id'=>$nome->id])}}"/>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <x-errors/>
     </div>
-    <x-errors/>
 </x-template>
