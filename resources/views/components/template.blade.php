a8a3c45a4bc7a4854cba13f47229e795136a1b0f
mateus-taiga <mateus.f.g.dacosta@outlook.com>
2024-05-18 17:22:44 -0300

    tentado colocar twillio mas o site ta fora

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link href="/assets/css/styles.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <title>{{$titulodapagina ?? 'Gestor de Email'}}</title>
+    <title>{{$titulodapagina ?? 'Gerenciador de Notificações'}}</title>
 </head>
 <body class="bg-dark">
 <header>
     <div class="container mt-4 mb-4 mx-auto d-flex justify-content-center align-items-center">
         <h1 class="text-white">
-            Gestor de Emails Automatico
+            Gerenciador de Notificações
         </h1>
     </div>
     <div class="container mt-4 mb-4 mx-auto d-flex justify-content-center align-items-center">
